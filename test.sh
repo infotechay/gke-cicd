96e983560aa50848e72433bdd29e3bb8877c4dd0
Ashish <infotechay@gmail.com>
2024-12-10 00:47:59 +0530

    13

---
+++ test.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+# Get the token from the first argument
+token="$1"
+
+# Echo the token for debugging (optional)
+echo "The token is: $token"
+
+curl -X GET -H "Authorization: token GITHUB_TOKEN1 \
+          https://api.github.com/repos/infotechay/gke-cicd/environments/mysecret1/secrets output.txt
+#!/bin/bash
+
+# Get the token from the first argument
+token="$1"
+
+# Echo the token for debugging (optional)
+echo "The token is: $token"

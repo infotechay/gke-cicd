bdae8c900c226c48cb00b5ab8402b29fc0540319
Ashish <infotechay@gmail.com>
2024-12-10 01:02:27 +0530

    13

--- script.sh
+++ script.sh
@@ -9,5 +9,5 @@
 # # Echo the token for debugging (optional)
 # echo "The token is: $token"
 
-curl -X GET -H "Authorization: token $GITHUB_TOKEN \
+curl -X GET -H "Authorization: token $GITHUB_TOKEN" \
           https://api.github.com/repos/infotechay/gke-cicd/environments/mysecret1/secrets >> output.txt
